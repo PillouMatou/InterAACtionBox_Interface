239231067e3c0d3e2d71ead10ebd0eb615d367a7
Riou Sebastien <rsebastien07@gmail.com>
2021-10-04 12:34:38 +0200

    Update interaactionBoxOS-linux.sh

--- src/main/bin/interaactionBoxOS-linux.sh
+++ src/main/bin/interaactionBoxOS-linux.sh
@@ -13,6 +13,22 @@ SCRIPT_DIR=$(dirname $0)
 echo "SCRIPT_DIR = ${SCRIPT_DIR}"  
   
 LIB_DIR=${SCRIPT_DIR}/../lib  
+  
+echo "LIB_DIR = ${LIB_DIR}"  
+  
+LIB_DIR_RELATIVE=$(realpath --relative-to="${WORKING_DIR}" "${LIB_DIR}")  
+  
+echo "LIB_DIR_RELATIVE = ${LIB_DIR_RELATIVE}"  
+  
+CLASSPATH=$(find ./$LIB_DIR_RELATIVE -name "*.jar" | sort | tr '\n' ':')  
+  
+export JAVA_HOME=${LIB_DIR}/jre  
+  
+echo "JAVA_HOME = ${JAVA_HOME}"  
+  
+export PATH=${JAVA_HOME}/bin:${PATH}  
+  
+echo "PATH = ${PATH}"
 
 FILE=${SCRIPT_DIR}/configuration.conf
 
@@ -29,6 +45,7 @@ fi;
 if [ -f "$FILE" ]; then
     echo "exists"
     gazePlaySave=$(head -n 1 "${FILE}")
+    echo "$gazePlaySave"
     if [ -d "$gazePlaySave" ]; then
       
       export JAVA_CMD="${WORKING_DIR}/../lib/jre/bin/java -cp \"$CLASSPATH\" ${JAVA_OPTS} main.Main $gazePlaySave"
