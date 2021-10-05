6db18e377edc094529250056026b1360ee7a479b
Riou Sebastien <sebastien.riou@univ-grenoble-alpes.fr>
2021-10-05 10:20:53 +0200

    Update interaactionBoxOS-linux.sh

--- bin/interaactionBoxOS-linux.sh
+++ bin/interaactionBoxOS-linux.sh
@@ -14,11 +14,7 @@ echo "SCRIPT_DIR = ${SCRIPT_DIR}"
   
 LIB_DIR=${SCRIPT_DIR}/../lib  
   
-echo "LIB_DIR = ${LIB_DIR}"
-
-JAVA_OPTS=-Djdk.gtk.version=2
-
-echo "JAVA_OPTS = ${JAVA_OPTS}"
+echo "LIB_DIR = ${LIB_DIR}"  
   
 LIB_DIR_RELATIVE=$(realpath --relative-to="${WORKING_DIR}" "${LIB_DIR}")  
   
@@ -34,11 +30,33 @@ export PATH=${JAVA_HOME}/bin:${PATH}
   
 echo "PATH = ${PATH}"
 
-gazePlaySave="../../gazeplay-linux-x64-1.9.1-AFSR"
-
 FILE=${SCRIPT_DIR}/configuration.conf
+if [ -f "$FILE" ]; then
+    echo "exists"
+    gazePlaySave=$(head -n 1 "${FILE}")
+    if [ -d "$gazePlaySave" ]; then
+      
+      export JAVA_CMD="${WORKING_DIR}/../lib/jre/bin/java -cp \"$CLASSPATH\" ${JAVA_OPTS} main.Main $gazePlaySave"
 
-echo "$gazePlaySave" > configuration.conf
+      echo "Executing command line: $JAVA_CMD"
+
+      ${JAVA_CMD}
+      
+      exit
+    fi
+fi
+
+gazePlaySave=""
+
+#while [ ! -d "$gazePlaySave" ] 
+#do
+#read -p "Entrez le chemin menant au dossier d'installation de GazePlay: " gazePlaySave
+#if [ ! -d "$gazePlaySave" ]; then
+# echo "Erreur: \"$gazePlaySave\" n'est pas un dossier valide."
+#fi
+# done
+
+#echo "$gazePlaySave" > configuration.conf
   
 export JAVA_CMD="${WORKING_DIR}/../lib/jre/bin/java -cp \"$CLASSPATH\" ${JAVA_OPTS} main.Main $gazePlaySave"
 
