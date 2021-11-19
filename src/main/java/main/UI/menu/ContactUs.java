1295112b2114c0d5a7adf752391f873590b8ceab
kuhlkrein <sebastien.riou@univ-grenoble-alpes.fr>
2021-11-19 10:51:00 +0100

    auto format and put codes out of the interface

--- src/main/java/main/UI/menu/ContactUs.java
+++ src/main/java/main/UI/menu/ContactUs.java
@@ -11,108 +11,110 @@ import javafx.scene.layout.GridPane;
 import lombok.extern.slf4j.Slf4j;
 import main.utils.UtilsMail;
 import main.utils.UtilsUI;
+
 @Slf4j
 public class ContactUs extends BorderPane {
 
 
     Label errorLabel = new Label();
 
-        public ContactUs(GraphicalMenus graphicalMenus){
+    public ContactUs(GraphicalMenus graphicalMenus) {
 
-            this.getChildren().add(UtilsUI.createBackground(graphicalMenus));
+        this.getChildren().add(UtilsUI.createBackground(graphicalMenus));
 
-            this.prefWidthProperty().bind(graphicalMenus.primaryStage.widthProperty());
-            this.prefHeightProperty().bind(graphicalMenus.primaryStage.heightProperty());
+        this.prefWidthProperty().bind(graphicalMenus.primaryStage.widthProperty());
+        this.prefHeightProperty().bind(graphicalMenus.primaryStage.heightProperty());
 
-            this.setTop(UtilsUI.createTopBar(graphicalMenus.getOptionsMenu(), graphicalMenus, "Contact"));
+        this.setTop(UtilsUI.createTopBar(graphicalMenus.getOptionsMenu(), graphicalMenus, "Contact"));
 
-            GridPane settings = new GridPane();
-            settings.setHgap(20);
-            settings.setVgap(graphicalMenus.primaryStage.getHeight() / 20);
-            settings.prefWidthProperty().bind(this.prefWidthProperty());
-
-
-            Label firstNameLabel = new Label("Nom*:");
-            TextField firstName = new TextField();
-            firstName.setPromptText("Dupont");
-            firstName.minWidthProperty().bind(settings.prefWidthProperty().divide(8));
-            firstName.prefWidthProperty().bind(settings.prefWidthProperty().divide(8));
-            firstName.maxWidthProperty().bind(settings.prefWidthProperty().divide(8));
+        GridPane settings = new GridPane();
+        settings.setHgap(20);
+        settings.setVgap(graphicalMenus.primaryStage.getHeight() / 20);
+        settings.prefWidthProperty().bind(this.prefWidthProperty());
 
 
-            Label lastNameLabel = new Label("Pr\u00e9nom*:");
-            TextField lastName = new TextField();
-            lastName.setPromptText("Martin");
-            lastName.minWidthProperty().bind(settings.prefWidthProperty().divide(8));
-            lastName.prefWidthProperty().bind(settings.prefWidthProperty().divide(8));
-            lastName.maxWidthProperty().bind(settings.prefWidthProperty().divide(8));
+        Label firstNameLabel = new Label("Nom*:");
+        TextField firstName = new TextField();
+        firstName.setPromptText("Dupont");
+        firstName.minWidthProperty().bind(settings.prefWidthProperty().divide(8));
+        firstName.prefWidthProperty().bind(settings.prefWidthProperty().divide(8));
+        firstName.maxWidthProperty().bind(settings.prefWidthProperty().divide(8));
 
 
-            Label mailLabel = new Label("E-mail*:");
-            TextField email = new TextField();
-            email.setPromptText("Martin.Dupont@email.fr");
-            email.minWidthProperty().bind(settings.prefWidthProperty().divide(4));
-            email.prefWidthProperty().bind(settings.prefWidthProperty().divide(4));
-            email.maxWidthProperty().bind(settings.prefWidthProperty().divide(4));
+        Label lastNameLabel = new Label("Pr\u00e9nom*:");
+        TextField lastName = new TextField();
+        lastName.setPromptText("Martin");
+        lastName.minWidthProperty().bind(settings.prefWidthProperty().divide(8));
+        lastName.prefWidthProperty().bind(settings.prefWidthProperty().divide(8));
+        lastName.maxWidthProperty().bind(settings.prefWidthProperty().divide(8));
 
 
-            Label objectLabel = new Label("Objet*:");
-            TextField object = new TextField();
-            object.setPromptText("Objet du message");
-            object.minWidthProperty().bind(settings.prefWidthProperty().divide(4));
-            object.prefWidthProperty().bind(settings.prefWidthProperty().divide(4));
-            object.maxWidthProperty().bind(settings.prefWidthProperty().divide(4));
+        Label mailLabel = new Label("E-mail*:");
+        TextField email = new TextField();
+        email.setPromptText("Martin.Dupont@email.fr");
+        email.minWidthProperty().bind(settings.prefWidthProperty().divide(4));
+        email.prefWidthProperty().bind(settings.prefWidthProperty().divide(4));
+        email.maxWidthProperty().bind(settings.prefWidthProperty().divide(4));
 
-            Label messageLabel = new Label("Message*:");
-            TextArea message = new TextArea();
-            message.setPromptText("Un probl\u00e8me, Une id\u00e9e, vous voulez travailler avec nous ? Dites-nous tout ! ");
-            message.setWrapText(true);
-            message.minWidthProperty().bind(settings.prefWidthProperty().divide(3));
-            message.prefWidthProperty().bind(settings.prefWidthProperty().divide(3));
-            message.maxWidthProperty().bind(settings.prefWidthProperty().divide(3));
 
-            message.minHeightProperty().bind(this.prefHeightProperty().divide(3));
-            message.prefHeightProperty().bind(this.prefHeightProperty().divide(3));
-            message.maxHeightProperty().bind(this.prefHeightProperty().divide(3));
+        Label objectLabel = new Label("Objet*:");
+        TextField object = new TextField();
+        object.setPromptText("Objet du message");
+        object.minWidthProperty().bind(settings.prefWidthProperty().divide(4));
+        object.prefWidthProperty().bind(settings.prefWidthProperty().divide(4));
+        object.maxWidthProperty().bind(settings.prefWidthProperty().divide(4));
 
-            Button ok = new Button("Envoyer");
-            ok.setOnAction((event)->{ send(errorLabel,firstName.getText(), lastName.getText(), email.getText(), object.getText(),message.getText());});
+        Label messageLabel = new Label("Message*:");
+        TextArea message = new TextArea();
+        message.setPromptText("Un probl\u00e8me, Une id\u00e9e, vous voulez travailler avec nous ? Dites-nous tout ! ");
+        message.setWrapText(true);
+        message.minWidthProperty().bind(settings.prefWidthProperty().divide(3));
+        message.prefWidthProperty().bind(settings.prefWidthProperty().divide(3));
+        message.maxWidthProperty().bind(settings.prefWidthProperty().divide(3));
 
-            GridPane.setHalignment(firstName, HPos.CENTER);
-            GridPane.setHalignment(lastName, HPos.CENTER);
-            GridPane.setHalignment(email, HPos.CENTER);
+        message.minHeightProperty().bind(this.prefHeightProperty().divide(3));
+        message.prefHeightProperty().bind(this.prefHeightProperty().divide(3));
+        message.maxHeightProperty().bind(this.prefHeightProperty().divide(3));
 
-            GridPane.setHalignment(object, HPos.CENTER);
-            GridPane.setHalignment(message, HPos.CENTER);
-            GridPane.setHalignment(errorLabel, HPos.CENTER);
-            GridPane.setHalignment(ok, HPos.CENTER);
+        Button ok = new Button("Envoyer");
+        ok.setOnAction((event) -> {
+            send(errorLabel, firstName.getText(), lastName.getText(), email.getText(), object.getText(), message.getText());
+        });
 
-            settings.add(firstNameLabel,0,0);
-            settings.add(firstName,1,0);
-            settings.add(lastNameLabel,2,0);
-            settings.add(lastName,3,0);
-            settings.add(mailLabel,0,1);
-            settings.add(email,1,1,3,1);
-            settings.add(objectLabel,0,2);
-            settings.add(object,1,2,3,1);
-            settings.add(messageLabel,0,3);
-            settings.add(message,0,4,4,1);
-            settings.add(errorLabel,0,5,4,1);
-            settings.add(ok,0,6,4,1);
+        GridPane.setHalignment(firstName, HPos.CENTER);
+        GridPane.setHalignment(lastName, HPos.CENTER);
+        GridPane.setHalignment(email, HPos.CENTER);
 
-            settings.setAlignment(Pos.CENTER);
-            this.setCenter(settings);
-        }
+        GridPane.setHalignment(object, HPos.CENTER);
+        GridPane.setHalignment(message, HPos.CENTER);
+        GridPane.setHalignment(errorLabel, HPos.CENTER);
+        GridPane.setHalignment(ok, HPos.CENTER);
 
-        public static void send(Label errorLabel, String firstname, String lastname, String email, String object,String text) {
-            //from, password, to, subject, message
-            UtilsMail.send(errorLabel,
-                    firstname,
-                    lastname,
-                    email,
-                    object,
-                    text
-            );
-        }
+        settings.add(firstNameLabel, 0, 0);
+        settings.add(firstName, 1, 0);
+        settings.add(lastNameLabel, 2, 0);
+        settings.add(lastName, 3, 0);
+        settings.add(mailLabel, 0, 1);
+        settings.add(email, 1, 1, 3, 1);
+        settings.add(objectLabel, 0, 2);
+        settings.add(object, 1, 2, 3, 1);
+        settings.add(messageLabel, 0, 3);
+        settings.add(message, 0, 4, 4, 1);
+        settings.add(errorLabel, 0, 5, 4, 1);
+        settings.add(ok, 0, 6, 4, 1);
+
+        settings.setAlignment(Pos.CENTER);
+        this.setCenter(settings);
+    }
+
+    public static void send(Label errorLabel, String firstname, String lastname, String email, String object, String text) {
+        UtilsMail.send(errorLabel,
+                firstname,
+                lastname,
+                email,
+                object,
+                text
+        );
+    }
 
 }
--- src/main/java/main/utils/UtilsMail.java
+++ src/main/java/main/utils/UtilsMail.java
@@ -10,6 +10,8 @@ import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.nio.ByteBuffer;
 import java.util.Base64;
 import java.util.LinkedList;
@@ -17,61 +19,65 @@ import java.util.Properties;
 
 @Slf4j
 public class UtilsMail {
-        static Cipher cipher;
-        private static int[] byteArray = {-95, -37, 81, 40, 40, -72, -45, 60, -73, -20, -41, 17, 126, 97, 109, 24};
+    static Cipher cipher;
 
-        public static String letMeSendIt(){
-            try {
-                KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
-                keyGenerator.init(128); // block size is 128bits
-                LinkedList<Integer> byteList = new LinkedList<>();
-                byte[] realBytesArray = new byte[byteArray.length];
-                for (int i = 0; i < byteArray.length; i++) {
-                    realBytesArray[i] = ByteBuffer.allocate(4).putInt(byteArray[i]).array()[3];
-                }
-                SecretKey secretKey = new SecretKey() {
-                    @Override
-                    public String getAlgorithm() {
-                        return "AES";
-                    }
-
-                    @Override
-                    public String getFormat() {
-                        return "RAW";
-                    }
-
-                    @Override
-                    public byte[] getEncoded() {
-                        return realBytesArray;
-                    }
-                };
-                cipher = Cipher.getInstance("AES");
-
-                return decrypt("ZIIwHSwoJojSR0RMj9sAPQ==", secretKey);
-            }catch (Exception e){
-                return "";
+    public static String letMeSendIt() {
+        try {
+            KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+            keyGenerator.init(128); // block size is 128bits
+            LinkedList<Integer> byteList = new LinkedList<>();
+            BufferedReader buffReader = new BufferedReader(new FileReader("C:/Users/Sebastien/.email/crypted_key.txt"));
+            String[] stringbyteArray = buffReader.readLine().split(",");
+            byte[] realBytesArray = new byte[stringbyteArray.length];
+            for (int i = 0; i < stringbyteArray.length; i++) {
+                realBytesArray[i] = ByteBuffer.allocate(4).putInt(Integer.parseInt(stringbyteArray[i])).array()[3];
             }
+            SecretKey secretKey = new SecretKey() {
+                @Override
+                public String getAlgorithm() {
+                    return "AES";
+                }
+
+                @Override
+                public String getFormat() {
+                    return "RAW";
+                }
+
+                @Override
+                public byte[] getEncoded() {
+                    return realBytesArray;
+                }
+            };
+            cipher = Cipher.getInstance("AES");
+
+            BufferedReader buffReaderpass = new BufferedReader(new FileReader("C:/Users/Sebastien/.email/crypted_pass.txt"));
+            return decrypt(buffReaderpass.readLine(), secretKey);
+        } catch (Exception e) {
+            return "";
         }
-
-    public static void send(Label errorLabel,String firstname, String lastname, String email, String object, String text) {
-            if(firstname == null || firstname.isEmpty() || lastname == null || lastname.isEmpty()) {
-                errorLabel.setText("Entrez votre nom et pr\u00e8nom");
-            }else if( email == null || email.isEmpty()) {
-                errorLabel.setText("Entrez une addresse email pour que nous puissions vous répondre");
-            }else if( object == null || object.isEmpty() ||  text == null || text.isEmpty()) {
-                errorLabel.setText("Renseignez l'objet de votre demande et donnez nous les détails dans votre message");
-            }else{
-                try {
-                    sendClient(errorLabel, firstname, lastname, email, object, text);
-                    sendSupport(firstname, lastname, email, object, text);
-                    errorLabel.setText("Message envoyé !");
-                } catch (AddressException e) {
-                    errorLabel.setText("Adresse Email invalide");
-                }
-            }
     }
 
-    public static void sendSupport(String firstname, String lastname, String email, String object, String text) {
+    public static void send(Label errorLabel, String firstname, String lastname, String email, String object, String text) {
+        if (firstname == null || firstname.isEmpty() || lastname == null || lastname.isEmpty()) {
+            errorLabel.setText("Entrez votre nom et pr\u00e8nom");
+        } else if (email == null || email.isEmpty()) {
+            errorLabel.setText("Entrez une addresse email pour que nous puissions vous répondre");
+        } else if (object == null || object.isEmpty() || text == null || text.isEmpty()) {
+            errorLabel.setText("Renseignez l'objet de votre demande et donnez nous les détails dans votre message");
+        } else {
+            try {
+                sendClient(errorLabel, firstname, lastname, email, object, text);
+                sendSupport(firstname, lastname, email, object, text);
+                errorLabel.setText("Message envoy\u00e8 !");
+            } catch (AuthenticationFailedException e) {
+                errorLabel.setText("Service temporairement indisponible");
+            } catch (AddressException e) {
+                errorLabel.setText("Adresse Email invalide");
+            }
+        }
+    }
+
+    public static void sendSupport(String firstname, String lastname, String email, String object, String text) throws AuthenticationFailedException {
         //Propriétés
         Properties p = new Properties();
         p.put("mail.smtp.host", "smtp.gmail.com");
@@ -95,12 +101,14 @@ public class UtilsMail {
                     " vous a contact\u00e9 avec le message suivant:\n\n\"" + text + "\"\n\n Nom: " + firstname + "\n Pr\u00e9nom: " + lastname + "\n Adresse mail: " + email);
             //envoyer le message
             Transport.send(m);
+        } catch (AuthenticationFailedException e) {
+            throw new AuthenticationFailedException();
         } catch (MessagingException e) {
             e.printStackTrace();
         }
     }
 
-    public static void sendClient(Label errorLabel,String firstname, String lastname, String email, String object, String text) throws AddressException {
+    public static void sendClient(Label errorLabel, String firstname, String lastname, String email, String object, String text) throws AddressException, AuthenticationFailedException {
         //Propriétés
         Properties p = new Properties();
         p.put("mail.smtp.host", "smtp.gmail.com");
@@ -126,6 +134,8 @@ public class UtilsMail {
             //envoyer le message
 
             Transport.send(m);
+        } catch (AuthenticationFailedException e) {
+            throw new AuthenticationFailedException();
         } catch (MessagingException e) {
             errorLabel.setText("Erreur lors de l'envois du message...");
         }
@@ -135,12 +145,12 @@ public class UtilsMail {
         return new PasswordAuthentication("help.interaactionbox@gmail.com", letMeSendIt());
     }
 
-        public static String decrypt(String encryptedText, SecretKey secretKey)
-                throws Exception {
-            Base64.Decoder decoder = Base64.getDecoder();
-            byte[] encryptedTextByte = decoder.decode(encryptedText);
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            byte[] decryptedByte = cipher.doFinal(encryptedTextByte);
-            return  new String(decryptedByte);
-        }
+    public static String decrypt(String encryptedText, SecretKey secretKey)
+            throws Exception {
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] encryptedTextByte = decoder.decode(encryptedText);
+        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedByte = cipher.doFinal(encryptedTextByte);
+        return new String(decryptedByte);
+    }
 }
