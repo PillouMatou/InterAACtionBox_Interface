c74d064cb916731296f907eb59bd8839ab298d91
Noars <55838787+Noars@users.noreply.github.com>
2021-12-16 14:35:10 +0100

    rename file sh

--- scripts/interAACtionGaze_windowId.sh
+++ scripts/interAACtionGaze_windowId.sh
@@ -3,7 +3,7 @@ rm -f interaactionGaze_windowId.txt
 var=$(xdotool search --onlyvisible --name InteraactionGaze)
 if [ -z "$var" ] || [ "$var" == "" ]
 then
-  sh ~/InterAACtionGaze/bin/interaactionPicto-linux.sh
+  sh ~/InterAACtionGaze/bin/interAACtionGaze-linux.sh
 else
   echo "$var" >interaactionGaze_windowId.txt
   xdotool windowactivate  "$var"
