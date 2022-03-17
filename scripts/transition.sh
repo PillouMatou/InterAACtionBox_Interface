5549cdfbc9aa8bb2da2afff2b82cafacfb0e0dd5
Noars <55838787+Noars@users.noreply.github.com>
2022-03-17 15:07:02 +0100

    Add new bouton

--- scripts/transition.sh
+++ scripts/transition.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-(echo "<h1><center>Encore un petit effort ^^ !</center></h1><img src=\"data:"
+actualLanguage=$LANG
+
+title="Calibration"
+text="Just a little more effort !"
+okButton="Next"
+cancelButton="Cancel"
+
+if [ "$actualLanguage" = "fr_FR.UTF-8" ]; then
+	title="Calibration"
+	text="Encore un petit effort !"
+	okButton="Suivant"
+	cancelButton="Annuler"
+fi
+
+(echo "<h1><center>$text</center></h1><img src=\"data:"
  mimetype -b "$1"
  echo -n ";base64,"
  base64 "$1"
- echo "\">") | zenity --text-info --html --filename=/dev/stdin --width=530 --height=425 --timeout=5 --title="Calibration"
+ echo "\">") | zenity --text-info --html --filename=/dev/stdin --width=530 --height=425 --timeout=5 --title="$title" --ok-label "$okButton" --cancel-label "$cancelButton"
